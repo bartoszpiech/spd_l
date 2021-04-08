4df9ff514d6a4d57d0cdcac29c1a83cdbb5780c2
Bartosz Piech <piech.bartosz696@gmail.com>
2021-04-08 18:58:04 +0200

    lab 3 update

---
+++ lab_3/main.cpp
@@ -0,0 +1,224 @@
+﻿#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+const int P_MIN = 1;
+const int P_MAX = 29;
+
+
+/*
+ * klasa generujaca liczby pseudolosowe
+ */
+class RandomNumberGenerator
+{
+private:
+
+	long seed;
+
+public:
+	RandomNumberGenerator(long seedValue) :
+		seed(seedValue)
+	{}
+	int nextInt(int low, int high) {
+		long k;
+		double value_0_1;
+		long m = 2147483647l, a = 16807l, b = 127773l, c = 2836l;
+
+		k = seed / b;
+		seed = a * (seed % b) - k * c;
+		if (seed < 0)
+			seed = seed + m;
+		value_0_1 = seed;
+		value_0_1 /= m;
+		return low + (int)floor(value_0_1 * (high - low + 1));
+	}
+	float nextFloat(int low, int high) {
+		low *= 100000;
+		high *= 100000;
+		float val = nextInt(low, high) / 100000.0;
+		return val;
+	}
+};
+
+/*
+class FSP {
+	public:
+		int id;		// indeks
+		int *p;
+
+		FSP(int i, RandomNumberGenerator &rng) {
+		*/
+
+
+/*
+ * klasa zawierajaca wartosci problemu jednomaszynowego
+ */
+class RPQ {
+private:
+	int id; // ideks instancji
+	int p;  // czas realizacji
+	int r;  // czas przygotowania
+	
+public:
+	RPQ(int a, int i) :
+		id(i),
+		p(a)
+	{}
+	int S = 0;	// czas oczekiwania w symulacji
+	int C = 0;	// czas realizacji w symulacji
+
+	void make_r(int b) { r = b; }
+	int _p() { return p; }
+	int _r() { return r; }
+	int _id() { return id; }
+};
+
+/*
+ * struktura pomocnicza (komparator)
+ */
+struct less_than_key{
+	inline bool operator() (RPQ& class1, RPQ& class2){
+		return (class1._r() < class2._r());
+	}
+};
+
+/*
+ * funkcja zwracajaca maksimum dwoch liczb
+ */
+int max (int a, int b) {
+	return a > b ? a : b;
+}
+
+/*
+ * symulacja RPQ
+ */
+void simulate(vector<RPQ>& container){
+	container[0].S = container[0]._r();
+	container[0].C = container[0]._p() + container[0].S;
+	for (unsigned int i = 1; i < container.size(); i++) {
+		container[i].S = max(container[i]._r(), container[i - 1].C);
+		container[i].C = container[i]._p() + container[i].S;
+	}
+}
+
+void show_results(vector<RPQ> container){
+	cout << "nr:[\t";
+	for (auto i : container) {
+		cout << i._id() << "\t";
+	}
+	cout << "]\nS: [\t";
+	for (auto i : container) {
+		cout << i.S << "\t";
+	}
+	cout << "]\nC: [\t";
+	for (auto i : container) {
+		cout << i.C << "\t";
+	}
+	cout << "]\n\n";
+}
+
+void show_generated(vector<RPQ> container) {
+	cout << "nr:[\t";
+	for (auto i : container) {
+		cout << i._id() << "\t";
+	}
+	cout << "]\nr: [\t";
+	for (auto i : container) {
+		cout << i._r() << "\t";
+	}
+	cout << "]\np: [\t";
+	for (auto i : container) {
+		cout << i._p() << "\t";
+	}
+	cout << "]\n\n";
+}
+
+/*
+void print(int **p) {
+	for (int i = 0; i < size[0]; i++) {
+		cout << "[";
+		for (int j = 0; j < size[1]; j++) {
+			cout << p[i][j] << " ";
+		}
+		cout << "],";
+	}
+}
+*/
+
+int main() {
+	int seed;
+	int size[2];
+	cout << "Wprowadz ziarno: ";
+	cin >> seed;
+	cout << "Wprowadz rozmiar problemu: ";
+	cin >> size[0];
+	cin >> size[1];
+
+	RandomNumberGenerator rng(seed);
+	
+	int p[size[0]][size[1]];
+	int S[size[0]][size[1]];
+	int C[size[0]][size[1]];
+	for (int i = 0; i < size[0]; i++) {
+		for (int j = 0; j < size[1]; j++) {
+			p[i][j] = rng.nextInt(P_MIN, P_MAX);
+		}
+	}
+
+	for (int i = 0; i < size[0]; i++) {
+		cout << "[";
+		for (int j = 0; j < size[1]; j++) {
+			cout << p[i][j] << " ";
+		}
+		cout << "],";
+	}
+	cout << "\n";
+
+	S[0][0] = 0;
+	S[0][1] = p[0][0];
+	C[0][0] = S[0][1];
+	C[0][1] = S[0][1] + p[0][1];
+	for (int i = 1; i < size[0]; i++) {
+		for (int j = 0; j < size[1]; j++) {
+			S[i][j] = S[i-1][j] + p[i][j];
+		}
+	}
+
+	for (int i = 0; i < size[0]; i++) {
+		cout << "[";
+		for (int j = 0; j < size[1]; j++) {
+			cout << S[i][j] << " ";
+		}
+		cout << "],";
+	}
+
+	
+	/*
+	std::vector<RPQ> container;
+	int A = 0;
+
+	// generowanie instancji
+	for (int i = 0; i < _size; i++) {
+		container.emplace_back(rng.nextInt(P_MIN, P_MAX), i+1);
+		A += container[i]._p();
+	}
+	for (auto &i : container) {
+		i.make_r(rng.nextInt(R_MIN, A));
+	}
+	show_generated(container);
+
+	// symulowanie rezulatu
+	simulate(container);
+	show_results(container);
+
+	// optymalizacja i ponowne symulowanie rezultatu
+	sort(container.begin(), container.end(), less_than_key());
+	simulate(container);
+	show_results(container);
+	*/
+
+	return 0;
+}
