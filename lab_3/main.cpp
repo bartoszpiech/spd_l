0b21179263a85b701956a2895327c68edf662445
Bartosz Piech <piech.bartosz696@gmail.com>
2021-04-16 13:56:52 +0200

    modified structure of the project and added johnson's algorithm

--- lab_3/main.cpp
+++ lab_3/main.cpp
@@ -44,22 +44,22 @@ class FSP {
 	public:
 		int tasks;
 		int machines;
-		int **arr;
+		vector<int*>arr;
 
 		FSP(int t, int m) {
 			tasks = t;
 			machines = m;
-			arr = new int*[tasks];
+			arr.resize(t);
 			for (int i = 0; i < tasks; i++) {
 				arr[i] = new int[machines];
 			}
 		}
 		~FSP() {
-			for (int i = 0; i < tasks; i++) {
-				delete(arr[i]);
+			for (auto i : arr) {
+				delete(i);
 			}
-			delete(arr);
 		}
+
 		void generate(RandomNumberGenerator &rng) {
 			for (int i = 0; i < tasks; i++) {
 				for (int j = 0; j < machines; j++) {
@@ -81,29 +81,62 @@ class FSP {
 		}
 
 		int solve_cost() {
-			FSP c = FSP(this->tasks, this->machines);
-			c.arr[0][0] = this->arr[0][0];
-			for(int j = 1; j < this->machines; j++) {
-				c.arr[0][j] = c.arr[0][j-1] + this->arr[1][j-1];
+			FSP c = FSP(tasks, machines);
+			c.arr[0][0] = arr[0][0];
+			for(int j = 1; j < machines; j++) {
+				c.arr[0][j] = c.arr[0][j-1] + arr[0][j];
+				//cout << "c.arr[0]["<<j<<"]=" << c.arr[0][j] << "\n";
 			}
-			for (int i = 1; i < this->tasks; i++) {
-				c.arr[i][0] = c.arr[i-1][0] + this->arr[i][0];
-				for(int j = 1; j < this->machines; j++) {
-					c.arr[i][j] = max(c.arr[i][j-1], c.arr[i-1][j]) + this->arr[i][j];
+			for (int i = 1; i < tasks; i++) {
+				c.arr[i][0] = c.arr[i-1][0] + arr[i][0];
+				for(int j = 1; j < machines; j++) {
+					c.arr[i][j] = max(c.arr[i][j-1], c.arr[i-1][j]) + arr[i][j];
 				}
 			}
 			cout << "C: ";
 			c.print();
-			return c.arr[this->tasks - 1][this->machines - 1];
+			return c.arr[tasks - 1][machines - 1];
+		}
+
+		int get_index_of_shortest_execution_time() {
+			if (arr.size() < 0) {
+				cerr << "Blad, probujesz dostac element z pustej tablicy\n";
+				exit(1);
+			}
+			int result = 0;
+			int shortest = arr[0][0];
+			for(unsigned int i = 0; i < arr.size(); i++) {
+				for(int j = 0; j < machines; j++) {
+					if (shortest > arr[i][j]) {
+						shortest = arr[i][j];
+						result = i;
+					}
+				}
+			}
+			return result;
 		}
 
 };
 
-void johnsons_algorithm(FSP N) {
-	int l = 1;
-	int k = N.tasks;
-	
-
+/*
+ * to do - dodac wersje dla wiekszej liczby maszyn niz 3
+ */
+FSP johnsons_algorithm(FSP N) {
+	FSP pi(N.tasks, N.machines);
+	int l = 0;	// zaczynamy od 0-ego elementu nie od 1 jak w pseudokodzie
+	int k = N.tasks - 1;	// tu to samo
+	while (!N.arr.empty()) {
+		int j = N.get_index_of_shortest_execution_time();
+		if (N.arr[j][0] < N.arr[j][1]) {
+			pi.arr[l] = N.arr[j];
+			l++;
+		} else {
+			pi.arr[k] = N.arr[j];
+			k--;
+		}
+		N.arr.erase(N.arr.begin() + j);
+	}
+	return pi;
 }
 
 int max(int a, int b) {
@@ -119,13 +152,16 @@ int main() {
 	cin >> size[0] >> size[1];
 
 	RandomNumberGenerator rng(seed);
+	FSP vec(size[0], size[1]);
+	vec.generate(rng);
+	vec.print();
 
-	FSP p = FSP(size[0], size[1]);
+	cout << "C max: " << vec.solve_cost() << endl;
+	
+	FSP pi = johnsons_algorithm(vec);
+	cout << "Algorytm Johnsona:\n";
+	pi.print();
 
-	p.generate(rng);
-	p.print();
-
-	cout << "c max: " << p.solve_cost() << endl;
-
+	cout << "C max: " << pi.solve_cost() << endl;
 	return 0;
 }
